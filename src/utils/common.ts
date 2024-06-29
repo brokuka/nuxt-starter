10b5a3e6956d77c9fb5db059f173aa22d1b5958b
Ernest Manukyan <erik.manukyan2010@yandex.ru>
2024-06-30 00:26:48 +0400

    chore: adding get all branches utilite

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,5 +1,7 @@
 import cp from 'node:child_process'
 import util from 'node:util'
+import { OWNER, TEMPLATES_REPO } from './constants'
+import type { Branch } from './types'
 
 export function execCmd(cmd: string, cwd?: string) {
   const exec = util.promisify(cp.exec)
@@ -7,6 +9,33 @@ export function execCmd(cmd: string, cwd?: string) {
   return exec(cmd, { cwd })
 }
 
-export function transformObjectToArray(obj: Record<string, string>) {
-  return Object.keys(obj).map(key => `${key}@${obj[key]}`)
+export function transformObjectToArray(obj: Record<string, string | Record<string, string>>): string[] {
+  return Object.keys(obj).flatMap((key) => {
+    const value = obj[key]
+
+    if (typeof value === 'string') {
+      return `${key}@${value}`
+    }
+
+    return transformObjectToArray(value).map(pkg => pkg)
+  })
+}
+
+export async function getAllBraches() {
+  const repoBranchesUrl = `https://api.github.com/repos/${OWNER}/${TEMPLATES_REPO}/branches`
+
+  try {
+    const response = (await fetch(repoBranchesUrl))
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch repository branches')
+    }
+
+    const branches: Branch[] = await response.json()
+
+    return branches.map(branch => branch.name)
+  }
+  catch (error) {
+    console.error(error)
+  }
 }
